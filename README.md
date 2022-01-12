555e7726832af5f38afa5289fa897513d3156249
Dicksonrsg <dickson.el@gmail.com>
2022-01-12 07:54:17 -0300

    Read me text and game cell

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # TictactoePy
+
+A game for Python practice.
+
+Found here:
+https://dev.to/jamesshah/the-classic-tictactoe-game-in-python-cpi
---
+++ main.py
@@ -0,0 +1,22 @@
+#Two Player TicTacToe Game in Python
+''' We will make the board using dictionary 
+    in which keys will be the location(i.e : top-left,mid-right,etc.)
+    and initialliy it's values will be empty space and then after every move 
+    we will change the value according to player's choice of move. '''
+
+theBoard = {'7': ' ' , '8': ' ' , '9': ' ' ,
+            '4': ' ' , '5': ' ' , '6': ' ' ,
+            '1': ' ' , '2': ' ' , '3': ' ' }
+
+board_keys = []
+
+for key in theBoard:
+    board_keys.append(key)
+
+''' Function to print updated board '''
+def showBoard(board):
+    print(board['7'] + '|' + board['8'] + '|' + board['9'])
+    print('-+-+-')
+    print(board['4'] + '|' + board['5'] + '|' + board['6'])
+    print('-+-+-')
+    print(board['1'] + '|' + board['2'] + '|' + board['3'])
